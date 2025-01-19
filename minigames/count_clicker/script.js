7d25049b22140f13f42e42e7909f7bf3a0e251aa
mattwydra <matthewwydra@gmail.com>
2025-01-19 18:25:55 -0500

    Add dark mode for count counter

--- minigames/count_clicker/script.js
+++ minigames/count_clicker/script.js
@@ -5,6 +5,7 @@ const counterDisplay = document.getElementById("counter-display");
 const incrementBtn = document.getElementById("increment-btn");
 const decrementBtn = document.getElementById("decrement-btn");
 const clearBtn = document.getElementById("clear-btn");
+const themeToggle = document.getElementById("themeToggle");
 
 //initialize counter
 let count = 0;
@@ -23,4 +24,15 @@ decrementBtn.addEventListener("click", () => {
 clearBtn.addEventListener("click", () => {
   count = 0;
   counterDisplay.textContent = count;
+});
+
+themeToggle.addEventListener("click", () => {
+  const body = document.body;
+  const currentTheme = body.getAttribute("data-theme");
+
+  if (currentTheme === "light") {
+    body.setAttribute("data-theme", "dark");
+  } else {
+    body.setAttribute("data-theme", "light");
+  }
 });
